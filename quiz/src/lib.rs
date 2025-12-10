6c4361b813886a6e74697e9c633f92c0a71beb34
tmvkrpxl0 <tmvkrpxl0@proton.me>
2025-12-10 21:11:48 +0900

    제출 페이지 추가

--- quiz/src/lib.rs
+++ quiz/src/lib.rs
@@ -1,9 +1,27 @@
+#![warn(clippy::all)]
+
 use axum::body::Body;
 use axum::http::{HeaderValue, StatusCode, header};
-use axum::response::{Html, Response};
+use axum::response::{Html, IntoResponse, Response};
+use chrono::Local;
 use question_macro::include_questions;
+use rand::TryRngCore;
+use rand::rngs::OsRng;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use tokio::fs::{File, create_dir_all};
+use tokio::io::AsyncWriteExt;
+use tokio::sync::RwLock;
+use tokio::task::JoinSet;
+use tracing::error;
+use url::form_urlencoded::byte_serialize;
 
-static TEMPLATE: &'static str = include_str!("../template.html");
+
+pub const FIVE_MINUTES: Duration = Duration::from_mins(5);
+static QUIZ_TEMPLATE: &str = include_str!("../question_template.html");
+static FINISH_TEMPLATE: &str = include_str!("../finish_template.html");
 pub static QUESTIONS: &[Question] = include_questions!("questions");
 
 pub static ICON: &[u8] = include_bytes!("../favicon.png");
@@ -63,7 +81,7 @@ pub fn render_question(question: &Question, index: usize) -> Html<String> {
     };
 
     Html::from(
-        TEMPLATE
+        QUIZ_TEMPLATE
             .replace("QUESTION_NAME", question.name)
             .replace("QUESTION_DESCRIPTION", &description)
             .replace("QUESTION_CODE", &code)
@@ -86,7 +104,7 @@ pub fn render_redirect() -> Html<String> {
 
     <meta property="og:site_name" content="코딩랩 Rust 스터디 중간평가"/>
     <meta property="og:title" content="코딩랩 Rust 스터디 중간평가"/>
-    <link rel="icon" href="/favicon.png"/>
+    <link rel="icon" href="./favicon.png"/>
 </head>
 <body>
 <script>
@@ -107,7 +125,7 @@ const defaultCorrect = Array(RANGE_SIZE).fill(false)
 document.cookie = "testSequence=" + encodeURIComponent(JSON.stringify(newSequence))
 document.cookie = "testSubmitted=" + encodeURIComponent(JSON.stringify(defaultSubmitted))
 document.cookie = "testCorrect=" + encodeURIComponent(JSON.stringify(defaultCorrect))
-document.location.href = "/" + newSequence[0]
+document.location.href = "./" + newSequence[0]
 </script>
 </body>
 </html>
@@ -117,18 +135,85 @@ document.location.href = "/" + newSequence[0]
     Html::from(contents)
 }
 
-pub async fn serve_file(file_name: &str) -> Response {
+pub fn render_finish_page(oauth_provider: Option<Arc<DiscordData>>) -> Html<String> {
+    static DISCORD_UNAVAILABLE: &'static str = r#"
+        <p id="no_discord" style="display: none">:p</p>
+        <p style="color: yellow; font-family: 'Miracode',serif; font-weight: 600; font-size: 48px">☹</p>
+        <p style="color: white; font-family: 'Pretendard Variable',serif; font-weight: 600; font-size: 48px">디스코드 API가 비활성화 되어 있습니다! 온라인 제출이 불가능합니다!</p>
+        <p style="color: white; font-family: 'Pretendard Variable',serif; font-weight: 600; font-size: 24px">퀴즈 결과:</p>
+"#;
 
+    static OFFLINE_SCRIPT: &'static str = r#"
+    {
+        let cookieObject = getCookie()
+        if (cookieObject == null) {
+            document.location.href = "./"
+        }
+    }
+    const resultList = document.getElementById("testResult")
+    for (let i = 0; i < QUESTION_COUNT; i++) {
+        const number = cookieObject.sequence[i]
+        const result = cookieObject.correct[i]
+
+        const text = document.createElement("a")
+        text.textContent = number
+        text.style.fontFamily = "Miracode"
+        text.style.display = "inline-block"
+        text.style.padding = "10px"
+        text.href = "./" + number
+        if (result) {
+            text.style.color = "green"
+        } else {
+            text.style.color = "red"
+        }
+        resultList.appendChild(text)
+    }
+"#;
+
+    static DISCORD_AVAILABLE: &'static str = r#"
+    <p style="color: royalblue; font-family: 'Pretendard Variable',serif; font-weight: 300; font-size: 32px; cursor: pointer", onclick="submit()">제출하기</p>
+"#;
+    static ONLINE_SCRIPT: &'static str = r#"
+    {
+        let cookieObject = getCookie()
+        if (cookieObject == null) {
+            document.location.href = "./"
+        }
+    }
+
+    function submit() {
+        const cookieObject = getCookie()
+        const jsonBody = JSON.stringify(cookieObject)
+        fetch("./submit", {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: jsonBody
+        }).then(response => response.text())
+        .then(text => window.location.href = text)
+    }
+"#;
+
+    let (submit, script) = match oauth_provider {
+        None => (DISCORD_UNAVAILABLE, OFFLINE_SCRIPT),
+        Some(_) => (DISCORD_AVAILABLE, ONLINE_SCRIPT),
+    };
+
+    let contents = FINISH_TEMPLATE
+        .replace("LARGE_MESSAGE", "수고하셨습니다!")
+        .replace("INIT_SCRIPT", script)
+        .replace("QUESTION_COUNT", QUESTIONS.len().to_string().as_str())
+        .replace("SUBMIT", submit);
+    Html::from(contents)
+}
+
+pub async fn serve_file(file_name: &str) -> Response {
     let (content_type, bytes) = match file_name {
         "favicon.png" => ("image/png", ICON),
         "Miracode.ttf" => ("font/ttf", MIRACODE),
         "PretendardVariable.woff2" => ("font/woff2", PRETENDARD_VARIABLE),
-        _ => {
-            return Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::empty())
-                .unwrap();
-        }
+        _ => return StatusCode::NOT_FOUND.into_response(),
     };
 
     let body = Body::from(bytes);
@@ -138,6 +223,260 @@ pub async fn serve_file(file_name: &str) -> Response {
         .status(StatusCode::OK)
         .body(body)
         .unwrap()
+}
+
+pub async fn try_init_discord() -> Option<Arc<DiscordData>> {
+    let discord_client_id = std::env::var("DISCORD_CLIENT_ID")
+        .map_err(|e| {
+            error!(
+                "Unable to fetch environment variable DISCORD_CLIENT_ID: {:?}",
+                e
+            );
+        })
+        .ok()?;
+    let discord_secret = std::env::var("DISCORD_SECRET")
+        .map_err(|e| {
+            error!(
+                "Unable to fetch environment variable DISCORD_SECRET: {:?}",
+                e
+            );
+        })
+        .ok()?;
+    let discord_redirect = std::env::var("DISCORD_REDIRECT")
+        .map_err(|e| {
+            error!(
+                "Unable to fetch environment variable DISCORD_SECRET: {:?}",
+                e
+            );
+        })
+        .ok()?;
+    let discord_redirect_encoded: String = byte_serialize(discord_redirect.as_bytes()).collect();
+    let discord_guild_id = std::env::var("DISCORD_GUILD_ID")
+        .map_err(|e| {
+            error!(
+                "Unable to fetch environment variable DISCORD_GUILD_ID: {:?}",
+                e
+            );
+        })
+        .ok()?;
+
+    let discord_data = DiscordData {
+        client_id: discord_client_id,
+        secret: discord_secret,
+        redirect_uri: discord_redirect,
+        redirect_uri_encoded: discord_redirect_encoded,
+        guild_id: discord_guild_id,
+        oauth_attempts: Default::default(),
+        leaderboard: Default::default(),
+    };
+    Some(Arc::new(discord_data))
+}
+
+pub async fn save_answer(
+    discord: Arc<DiscordData>,
+    sender_id: String,
+    quiz_result: QuizResult,
+) -> Result<(), StatusCode> {
+    let json_string = serde_json::to_string_pretty(&quiz_result).unwrap();
+    let sender_id_clone = sender_id.clone();
+
+    let data_updater = async move || {
+        let data = discord.as_ref();
+        let mut writer = data.leaderboard.write().await;
+        writer.insert(sender_id, quiz_result);
+        Ok(())
+    };
+
+    let file_writer = async move || {
+        let sender_id = sender_id_clone;
+        let now = Local::now();
+        let filename = now.format("%Y-%m-%dT%H-%M-%S.json").to_string();
+
+        let dir_path = format!("submissions/{}", sender_id);
+        let file_path = format!("submissions/{}/{}", sender_id, filename);
+        if let Err(e) = create_dir_all(dir_path.as_str()).await {
+            error!(
+                "Unable to create submission directory at {}!\n{:?}\nData is not saved: {}",
+                dir_path, e, json_string
+            );
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        };
+
+        let mut file = match File::create(file_path.as_str()).await {
+            Ok(f) => f,
+            Err(e) => {
+                error!(
+                    "Unable to create submission file at {}!\n{:?}\nData is not saved: {}",
+                    file_path, e, json_string
+                );
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        };
+
+        if let Err(e) = file.write_all(json_string.as_bytes()).await {
+            error!(
+                "Unable to write submission entry at {}!\n{:?}\nData is not saved: {}",
+                file_path, e, json_string
+            );
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+
+        if let Err(e) = file.flush().await {
+            error!(
+                "Unable to save submission entry at {}!\n{:?}\nData is not saved: {}",
+                file_path, e, json_string
+            );
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        };
+
+        Ok(())
+    };
+
+    let mut join_set = JoinSet::new();
+    join_set.spawn(data_updater());
+    join_set.spawn(file_writer());
+    let result1 = join_set.join_next().await.unwrap().unwrap();
+    let result2 = join_set.join_next().await.unwrap().unwrap();
+
+    let mut result = result1;
+    if result.is_ok() {
+        result = result2;
+    }
+
+    result
+}
+
+pub async fn handle_submit(discord: Arc<DiscordData>, cookie: UserCookie) -> Response {
+    let mut rng = OsRng::default();
+    let mut salt = [0u8; 16];
+    rng.try_fill_bytes(&mut salt).unwrap();
+    let salt = u128::from_le_bytes(salt);
+    let salt_string = format!("{:X}", salt);
+    let url = format!(
+        "https://discord.com/oauth2/authorize?client_id={}&response_type=code&redirect_uri={}&state={}&scope=identify+guilds.members.read&prompt=none",
+        discord.client_id,
+        discord.redirect_uri_encoded.as_str(),
+        salt_string
+    );
+    debug_assert_eq!(cookie.correct.len(), cookie.sequence.len());
+    debug_assert_eq!(cookie.correct.len(), cookie.submitted.len());
+
+    let score = (cookie.correct.into_iter().filter(|v| *v).count() as f32) / cookie.submitted.len() as f32;
+    let answers: Result<Vec<_>, StatusCode> = cookie.sequence.into_iter().zip(cookie.submitted.into_iter()).map(|(sequence, entry)| {
+        let Some(question) = QUESTIONS.get(sequence) else {
+            return Err(StatusCode::BAD_REQUEST);
+        };
+        let name = format!("{}.md", question.name);
+        let answer_value = match entry.find(' ') {
+            Some(position) => {
+                entry[position + 1..].to_string()
+            }
+            None => {
+                let index: usize = entry.parse().unwrap();
+                let Some(choice) = question.choices.get(index) else {
+                    return Err(StatusCode::BAD_REQUEST);
+                };
+                match choice {
+                    Answer::Choice { label } => label.to_string(),
+                    Answer::Subjective { .. } => return Err(StatusCode::BAD_REQUEST),
+                }
+            }
+        };
+        Ok((name, answer_value))
+    }).collect();
+    let answers = match answers {
+        Ok(v) => v,
+        Err(e) => return e.into_response()
+    };
+    let quiz_result = QuizResult {
+        answers,
+        score,
+    };
+
+    let now = Instant::now();
+    discord.oauth_attempts.write().await.insert(salt, (now, quiz_result));
+
+    url.into_response()
+}
+
+pub async fn oauth_redirect(param: OauthRedirectUrlParams, discord: Arc<DiscordData>) -> Response {
+    let Ok(salt) = u128::from_str_radix(param.state.as_str(), 16) else {
+        return StatusCode::BAD_REQUEST.into_response();
+    };
+    let mut attempts_writer = discord.oauth_attempts.write().await;
+    let quiz_result = match attempts_writer.remove(&salt) {
+        None => return StatusCode::UNAUTHORIZED.into_response(),
+        Some((time, result)) => {
+            let now = Instant::now();
+            let duration = now.duration_since(time);
+
+            if duration > FIVE_MINUTES {
+                return StatusCode::UNAUTHORIZED.into_response();
+            }
+            result
+        }
+    };
+    drop(attempts_writer);
+
+    let client = reqwest::Client::new();
+    let request = client
+        .post("https://discord.com/api/v10/oauth2/token")
+        .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .body(format!(
+            "grant_type=authorization_code&code={}&redirect_uri={}",
+            param.code,
+            discord.redirect_uri_encoded.as_str()
+        ))
+        .basic_auth(
+            discord.client_id.as_str(),
+            Some(discord.secret.as_str()),
+        )
+        .build()
+        .unwrap();
+    let response = match client.execute(request).await {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Unable to communicate with discord server!:\n{:?}", e);
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        }
+    };
+    if response.status() != StatusCode::OK {
+        error!("Discord server returned code {:?}. Response Body: {}", response.status(), response.text().await.unwrap());
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    }
+
+    let response: OauthResponse = match response.json::<OauthResponse>().await {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Failed to decode discord server's response!:\n{:?}", e);
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        }
+    };
+    debug_assert!(response.scope.contains("guilds.members.read"));
+    debug_assert_eq!(response.token_type, "Bearer");
+
+    let guild_member = get_current_user_guild_profile(response.access_token.as_str(), discord.guild_id.as_str(), &client).await;
+    let name = guild_member.nick.or(guild_member.user.global_name).unwrap_or(guild_member.user.username);
+
+    let save_result = save_answer(discord, guild_member.user.id, quiz_result).await;
+    match save_result {
+        Ok(_) => {
+            Response::builder()
+                .header(header::CONTENT_TYPE, "text/plain; charset=utf-8")
+                .body(Body::from(format!("참여해주셔서 감사합니다, {}님!", name)))
+                .unwrap()
+        },
+        Err(code) => code.into_response()
+    }
+}
+
+pub async fn get_current_user_guild_profile(
+    access_token: &str,
+    guild_id: &str,
+    client: &reqwest::Client
+) -> DiscordGuildMember {
+    let url = format!("https://discord.com/api/users/@me/guilds/{}/member", guild_id);
+    client.get(&url).bearer_auth(access_token).send().await.unwrap().json::<DiscordGuildMember>().await.unwrap()
 }
 
 pub struct Question {
@@ -157,4 +496,109 @@ pub enum Answer {
         label: &'static str,
         value: &'static str,
     },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum SubmittedAnswer {
+    /// 객관식
+    Choice { label: String },
+
+    /// 주관식
+    Subjective { label: String, value: String },
+}
+
+pub type SubmissionData = HashMap<String, QuizResult>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QuizResult {
+    answers: Vec<(String, String)>,
+    score: f32
+}
+
+#[expect(dead_code)]
+pub struct DiscordData {
+    client_id: String,
+    secret: String,
+    redirect_uri: String,
+    redirect_uri_encoded: String,
+    guild_id: String,
+    pub oauth_attempts: RwLock<HashMap<u128, (Instant, QuizResult)>>,
+    leaderboard: RwLock<SubmissionData>,
+}
+
+pub struct ServiceState {
+    pub pre_rendered_questions: Vec<Html<String>>,
+    pub pre_rendered_redirect: Html<String>,
+    pub discord_data: Option<Arc<DiscordData>>,
+    pub pre_rendered_finish_page: Html<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct OauthRedirectUrlParams {
+    code: String,
+    state: String,
+}
+
+#[expect(dead_code)]
+#[derive(Deserialize, Debug)]
+pub struct OauthResponse {
+    token_type: String,
+    access_token: String,
+    expires_in: usize,
+    refresh_token: String,
+    scope: String,
+}
+
+#[expect(dead_code)]
+#[derive(Deserialize, Debug)]
+pub struct DiscordUser {
+    id: String,
+    username: String,
+    global_name: Option<String>,
+    avatar: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DiscordGuildMember {
+    user: DiscordUser,
+    nick: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserCookie {
+    correct: Vec<bool>,
+    sequence: Vec<usize>,
+    submitted: Vec<String>,
+}
+
+impl Question {
+    pub fn test(&self, answer: &SubmittedAnswer) -> bool {
+        self.choices[self.answer].eq(answer)
+    }
+}
+
+impl PartialEq<SubmittedAnswer> for Answer {
+    fn eq(&self, other: &SubmittedAnswer) -> bool {
+        match self {
+            Answer::Choice { label } => {
+                let SubmittedAnswer::Choice { label: label_other } = other else {
+                    return false;
+                };
+
+                label.trim().eq_ignore_ascii_case(label_other.trim())
+            }
+            Answer::Subjective { label, value } => {
+                let SubmittedAnswer::Subjective {
+                    label: label_other,
+                    value: value_other,
+                } = other
+                else {
+                    return false;
+                };
+
+                label.trim().eq_ignore_ascii_case(label_other.trim())
+                    && value.trim().eq_ignore_ascii_case(value_other.trim())
+            }
+        }
+    }
 }
--- quiz/src/main.rs
+++ quiz/src/main.rs
@@ -1,18 +1,30 @@
-use axum::Router;
-use axum::body::Body;
-use axum::extract::Path;
+#![warn(clippy::all)]
+
+use axum::extract::{Path, Query, State};
 use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
-use axum::routing::get;
+use axum::response::{IntoResponse};
+use axum::routing::{get, put};
+use axum::{Json, Router};
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::str::FromStr;
-use study_test::{QUESTIONS, render_question, render_redirect, serve_file};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use study_test::{handle_submit, oauth_redirect, render_finish_page, render_question, render_redirect, serve_file, try_init_discord, OauthRedirectUrlParams, ServiceState, UserCookie, FIVE_MINUTES, QUESTIONS};
 use tokio::net::TcpListener;
+use tokio::time::sleep;
 use tower_http::trace::TraceLayer;
-use tracing::info;
+use tracing::{info, warn};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{fmt, EnvFilter};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
     let pre_rendered_questions: Vec<_> = QUESTIONS
         .iter()
         .enumerate()
@@ -20,12 +32,45 @@ async fn main() {
         .collect();
     let pre_rendered_redirect = render_redirect();
 
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
+    let discord_data = try_init_discord().await;
+    if let None = discord_data {
+        warn!("Discord support disabled!");
+    }
+    let pre_rendered_finish_page = render_finish_page(discord_data.clone());
+    let state = ServiceState {
+        pre_rendered_questions,
+        pre_rendered_redirect,
+        discord_data,
+        pre_rendered_finish_page,
+    };
+    let state = Arc::new(state);
+
+    if state.discord_data.is_some() {
+        let state_clone = state.clone();
+
+        let oauth_state_cleaner = async move || {
+            let state = state_clone;
+            let leaderboard = state.discord_data.as_ref().unwrap();
+            loop {
+                let cutoff = Instant::now() - FIVE_MINUTES;
+                {
+                    let mut writer = leaderboard.oauth_attempts.write().await;
+                    writer.retain(|_, &mut (requested_time, _)| requested_time >= cutoff);
+                }
+                sleep(Duration::from_mins(1)).await;
+            }
+        };
+
+        tokio::spawn(oauth_state_cleaner());
+    }
 
     let app = Router::new()
-        .route("/", get(|| async { pre_rendered_redirect }))
+        .route(
+            "/",
+            get(async |State(state): State<Arc<ServiceState>>| {
+                state.pre_rendered_redirect.clone().into_response()
+            }),
+        )
         .route("/favicon.png", get(|| serve_file("favicon.png")))
         .route("/Miracode.ttf", get(|| serve_file("Miracode.ttf")))
         .route(
@@ -33,20 +78,48 @@ async fn main() {
             get(|| serve_file("PretendardVariable.woff2")),
         )
         .route(
-            "/{question}",
-            get(|path: Path<usize>| {
-                let index = path.0;
-                async move {
-                    match pre_rendered_questions.get(index) {
-                        Some(v) => v.clone().into_response(),
-                        None => {
-                            let mut response = Response::new(Body::from("Question not found"));
-                            *response.status_mut() = StatusCode::NOT_FOUND;
-                            response
-                        }
-                    }
+            "/finish",
+            get(async |State(state): State<Arc<ServiceState>>| {
+                state.pre_rendered_finish_page.clone().into_response()
+            }),
+        )
+        .route(
+            "/submit",
+            put(async |State(state): State<Arc<ServiceState>>, Json(cookie): Json<UserCookie>| {
+                match state.discord_data.as_ref() {
+                    None => StatusCode::NOT_FOUND.into_response(),
+                    Some(v) => handle_submit(v.clone(), cookie).await
                 }
             }),
+        )
+        .route(
+            "/oauth-redirect",
+            get(
+                async |param: Query<OauthRedirectUrlParams>, State(state): State<Arc<ServiceState>>| {
+                    match state.discord_data.as_ref() {
+                        None => StatusCode::NOT_FOUND.into_response(),
+                        Some(v) => oauth_redirect(param.0, v.clone()).await.into_response()
+                    }
+
+                },
+            ),
+        )
+        // .route("/submit", put())
+        .route(
+            "/{question}",
+            get(
+                |State(state): State<Arc<ServiceState>>, path: Path<usize>| {
+                    let index = path.0;
+                    async move {
+                        match state.pre_rendered_questions.get(index) {
+                            Some(v) => v.clone().into_response(),
+                            None => {
+                                StatusCode::NOT_FOUND.into_response()
+                            }
+                        }
+                    }
+                },
+            ),
         )
         .layer(TraceLayer::new_for_http());
 
@@ -57,5 +130,5 @@ async fn main() {
         .unwrap_or(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 8080));
     info!("Serving at {}", address);
     let listener = TcpListener::bind(address).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app.with_state(state)).await.unwrap();
 }
